1323444425e532caa11be22af6bbd2fcc04c36c9
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-08-21 10:09:38 +0300

    upd

--- parse3proba.py
+++ parse3proba.py
@@ -1,57 +1,86 @@
 import bs4
 import requests
 import openpyxl
+import time
 
 # <editor-fold desc="start code">
 
 
-def get_check_write(selector, column):
+def get_check_write(row, column, selector):
+    sheet.cell(row=row, column=1).value = user_id
+    sheet.cell(row=row, column=2).value = name
     try:
         value = soup.select(selector)[0].getText()
-        sheet.cell(row=row, column=column).value = value
+        if value.endswith('Дата вступу') \
+                or value.endswith('Дата Пластової присяги') \
+                or value.endswith('Дата Іменування розвідувачем') \
+                or value.endswith('Дата Іменування розвідувачем'):
+            sheet.cell(row=row, column=column).value = 'нема'
+        else:
+            if value.startswith('Дата вступу'):
+                value = value.replace('Дата вступу', '')
+            if value.startswith('Дата Пластової присяги'):
+                value = value.replace('Дата Пластової присяги', '')
+            if value.startswith('Дата Іменування розвідувачем'):
+                value = value.replace('Дата Іменування розвідувачем', '')
+            if value.startswith('Дата Іменування розвідувачем'):
+                value = value.replace('Дата Іменування розвідувачем', '')
+            sheet.cell(row=row, column=column).value = value
+
     except IndexError:
         sheet.cell(row=row, column=column).value = 'нема'
 
 
 wb = openpyxl.open('3 проба.xlsx')
 sheet = wb.active
+print('opening sheet')
 url = 'http://3proba.sitegist.net/uk/profile/?userid='
+# user_id_list = [2000, 7500, 1000, 7304, 7416]
+first_id = 365
+last_id = 400  # 7824
+cycle_start_time = time.time()
 
 # </editor-fold>
 
-user_id = 7430
-while user_id <= 7440:
-    for row in range(2, sheet.max_row):
-        for x in range(1, 7440):
 
-            page = requests.get(url + str(user_id))
-            page.raise_for_status()
-            soup = bs4.BeautifulSoup(page.text, 'html.parser')
-            try:
-                name = soup.select('div.profileBox > h3')[0].getText()
+for user_id in range(first_id, last_id + 1):
+    print(f'{user_id - (first_id - 1)} / {last_id - first_id}')
+    row = user_id - 363
+    page = requests.get(url + str(user_id))
+    page.raise_for_status()
+    soup = bs4.BeautifulSoup(page.text, 'html.parser')
 
-                # <editor-fold desc="не 3пробник">
-                try:
-                    if soup.select('div.well > b')[0].getText().startswith('Ти ще не третьопробник'):
-                        sheet.cell(row=row, column=1).value = user_id
-                        sheet.cell(row=row, column=2).value = name
-                        sheet.cell(row=row, column=3).value = 'не 3пробник'
-                except IndexError:
-                    print('нема блоку "ще не третьопробник"')
-                # </editor-fold>
+    # <editor-fold desc="scrapping and writing data from profile">
+    try:  # if there is no name = empty page
+        name = soup.select('div.profileBox > h3')[0].getText()
 
-                else:
-                    name_text = name[0].getText()
-                    print(name_text)
-                    sheet.cell(row=x, column=1).value = str(user_id)
-                    sheet.cell(row=x, column=2).value = name_text
+        get_check_write(row, 3, 'div.panel-body > p:nth-child(1)') # датаВступу
+        get_check_write(row, 4, 'div.panel-body > p:nth-child(2)') # датаПрисяги
+        get_check_write(row, 5, 'div.panel-body > p:nth-child(3)') # часНаПробу
+        get_check_write(row, 6, 'div.alert.alert-success') # датаІменуванняРозвідувачем
+        get_check_write(row, 7, 'div:nth-child(3) > div.panel-body') # діловедення
+        get_check_write(row, 8, 'div:nth-child(4) > div.panel-body') # табори
+        get_check_write(row, 9, 'div:nth-child(5) > div.panel-body') # пересторогиТаВідзначення
+        get_check_write(row, 10, 'div.well > b') # неТретьопробник
+        get_check_write(row, 11, 'div.well > div > div.num') # номерПроби
+        get_check_write(row, 12, 'div.well > div > div.date') # датаПроби
+        get_check_write(row, 13, 'div.descriptionCol > h4') # курінь
+        get_check_write(row, 14, '___') # станиця
+        get_check_write(row, 15, '___') # округа
 
-            except IndexError:
-                sheet.cell(row=row, column=1).value = user_id
-                sheet.cell(row=row, column=2).value = 'пуста сторінка'
+        vmilosti_string = '' # вмілості
+        vmilosti_element = soup.select('div.uservmilist > div')
+        for x in vmilosti_element:
+            vmilosti_string += x.getText() + ', '
+        sheet.cell(row=row, column=16).value = vmilosti_string.replace('\n', '')
 
-        user_id += 1
+    except IndexError:
+        sheet.cell(row=row, column=1).value = user_id
+        sheet.cell(row=row, column=2).value = 'пуста сторінка'
+    # </editor-fold>
 
 wb.save('3 проба.xlsx')
+cycle_time = round(time.time() - cycle_start_time, 2)
+print(cycle_time)
 
 
