b26cf208b7c86b90967e59851fc0ec11e1bd9c24
slavkoSenkiv <60449898+slavkoSenkiv@users.noreply.github.com>
2021-08-21 08:35:08 +0300

    upd

--- parse3proba.py
+++ parse3proba.py
@@ -2,33 +2,56 @@ import bs4
 import requests
 import openpyxl
 
+# <editor-fold desc="start code">
+
+
+def get_check_write(selector, column):
+    try:
+        value = soup.select(selector)[0].getText()
+        sheet.cell(row=row, column=column).value = value
+    except IndexError:
+        sheet.cell(row=row, column=column).value = 'нема'
+
+
 wb = openpyxl.open('3 проба.xlsx')
 sheet = wb.active
-
 url = 'http://3proba.sitegist.net/uk/profile/?userid='
+
+# </editor-fold>
 
 user_id = 7430
 while user_id <= 7440:
-    for x in range(1, 7440):
+    for row in range(2, sheet.max_row):
+        for x in range(1, 7440):
 
-        page = requests.get(url + str(user_id))
-        page.raise_for_status()
-        soup = bs4.BeautifulSoup(page.text, 'html.parser')
-        name = soup.select('#main > div.padding > div > div > div.col-sm-5 > div.panel.panel-default.profileBox > h3')
+            page = requests.get(url + str(user_id))
+            page.raise_for_status()
+            soup = bs4.BeautifulSoup(page.text, 'html.parser')
+            try:
+                name = soup.select('div.profileBox > h3')[0].getText()
 
-        if not name:
-            name_text = 'cant find name'
-            print('cant find name')
-            sheet.cell(row=x, column=1).value = str(user_id)
-            sheet.cell(row=x, column=2).value = name_text
+                # <editor-fold desc="не 3пробник">
+                try:
+                    if soup.select('div.well > b')[0].getText().startswith('Ти ще не третьопробник'):
+                        sheet.cell(row=row, column=1).value = user_id
+                        sheet.cell(row=row, column=2).value = name
+                        sheet.cell(row=row, column=3).value = 'не 3пробник'
+                except IndexError:
+                    print('нема блоку "ще не третьопробник"')
+                # </editor-fold>
 
-        else:
-            name_text = name[0].getText()
-            print(name_text)
-            sheet.cell(row=x, column=1).value = str(user_id)
-            sheet.cell(row=x, column=2).value = name_text
+                else:
+                    name_text = name[0].getText()
+                    print(name_text)
+                    sheet.cell(row=x, column=1).value = str(user_id)
+                    sheet.cell(row=x, column=2).value = name_text
+
+            except IndexError:
+                sheet.cell(row=row, column=1).value = user_id
+                sheet.cell(row=row, column=2).value = 'пуста сторінка'
 
         user_id += 1
-        wb.save('3 проба.xlsx')
+
+wb.save('3 проба.xlsx')
 
 
--- parseEplastOptimised.py
+++ parseEplastOptimised.py
@@ -13,17 +13,18 @@ sheet = wb.active
 
 
 def by_xpath(value_name, column, xpath):
-    start_time = time.time()
+    start_by_xpath_time = time.time()
     print(f'{value_name} ', end='')
     try:
         value = browser.find_element_by_xpath(f'{xpath}').text
     except NoSuchElementException:
         value = f'no {value_name}'
     sheet.cell(row=user_id + 2, column=column).value = value
-    print(round(time.time() - start_time, 2), end=' | ')
+    print(round(time.time() - start_by_xpath_time, 2), end=' | ')
+
 
 def by_class(value_name, column, class_name):
-    start_time = time.time()
+    start_by_class_time = time.time()
     print(f'{value_name} ', end='')
     try:
         list_of_values = browser.find_elements_by_class_name(f'{class_name}')
@@ -33,7 +34,8 @@ def by_class(value_name, column, class_name):
     except NoSuchElementException:
         value_string = f'no {value_name}'
     sheet.cell(row=user_id + 2, column=column).value = value_string
-    print(round(time.time() - start_time, 2), end=' | ')
+    print(round(time.time() - start_by_class_time, 2), end=' | ')
+
 # </editor-fold>
 
 # <editor-fold desc="log in into main page">
@@ -82,10 +84,9 @@ whole_scrapping_cycle_start_time = time.time()
 
 # <editor-fold desc="scrapping TA data">
 queue = 1
-# for user_id in range(len(users_ids_list)):
-for user_id in range(5):
-
-    print(f'\n{queue} / {str(len(users_ids_list))}')
+# for user_id in range(5):
+for user_id in range(len(users_ids_list)):
+    print(f'\n{queue} / {str(len(users_ids_list))}, id {users_ids_list[user_id]}')
     queue += 1
 
     # user_id
@@ -105,39 +106,38 @@ for user_id in range(5):
     # </editor-fold>
 
     # <editor-fold desc="social">
-    start_time = time.time()
+    """start_time = time.time()
     try:
         social = browser.find_element_by_xpath('/html/body/div/div[2]/div[1]/div[2]/div[4]/div[1]/div[5]/div[2]/a').get_attribute('href')
     except NoSuchElementException:
         social = 'no social'
     print('social ', end='')
     sheet.cell(row=user_id + 2, column=11).value = social
-    print(round(time.time() - start_time, 2), end=' | ')
+    print(round(time.time() - start_time, 2), end=' | ')"""
     # </editor-fold>
 
     by_class('kureni', 4, 'simpleunit')
     by_xpath('stupin', 5, '/html/body/div/div[2]/div[1]/div[2]/div[4]/div[1]/div[5]/div[2]/div[4]/div[2]')
-    by_xpath('upu_stupin', 6, '/html/body/div/div[2]/div[1]/div[2]/div[4]/div[1]/div[5]/div[2]/div[15]/div[2]')
-    by_class('awards', 10, 'award_item')
-    by_class('kvs', 9, 'kv_item')
-    by_xpath('education', 12, '/html/body/div/div[2]/div[1]/div[2]/div[4]/div[1]/div[5]/div[2]/div[10]/div[2]')
+    # by_xpath('upu_stupin', 6, '/html/body/div/div[2]/div[1]/div[2]/div[4]/div[1]/div[5]/div[2]/div[15]/div[2]')
+    # by_class('awards', 10, 'award_item')
+    # by_class('kvs', 9, 'kv_item')
+    # by_xpath('education', 12, '/html/body/div/div[2]/div[1]/div[2]/div[4]/div[1]/div[5]/div[2]/div[10]/div[2]')
 
     # membership
     membershipButton = browser.find_element_by_xpath('/html/body/div/div[2]/div[1]/div[2]/div[4]/div[1]/div[1]/ul/li[2]/a')
     membershipButton.click()
 
-    by_xpath('startdate', 3, '/html/body/div/div[2]/div/div[2]/div[4]/div/div[5]/div[2]/div[1]/div[2]')
-    by_xpath('oathdate', 7, '/html/body/div/div[2]/div/div[2]/div[4]/div/div[5]/div[2]/div[2]/div[2]')
-    by_xpath('stpl_stupin_date', 8, '/html/body/div/div[2]/div/div[2]/div[4]/div/div[5]/table/tbody/tr/td[3]')
+    # by_xpath('startdate', 3, '/html/body/div/div[2]/div/div[2]/div[4]/div/div[5]/div[2]/div[1]/div[2]')
+    # by_xpath('oathdate', 7, '/html/body/div/div[2]/div/div[2]/div[4]/div/div[5]/div[2]/div[2]/div[2]')
+    # by_xpath('stpl_stupin_date', 8, '/html/body/div/div[2]/div/div[2]/div[4]/div/div[5]/table/tbody/tr/td[3]')
     # by_xpath('profession', 13, '/html/body/div/div[2]/div[1]/div[2]/div[4]/div[1]/div[5]/div[2]/div[13]/div[2]')
-    by_xpath('2enterDate', 14, '/html/body/div/div[2]/div/div[2]/div[4]/div/div[5]/div[2]')
+    # by_xpath('2enterDate', 14, '/html/body/div/div[2]/div/div[2]/div[4]/div/div[5]/div[2]')
     # by_xpath('end_of_aprobation', 15, '/html/body/div/div[2]/div/div[2]/div[4]/div/div[5]/div[3]')
 
 # </editor-fold>
 
 wb.save('епласт Львів хлопці oптимізовано.xlsx')
-whole_scrapping_cycle_time = round(time.time() - whole_scrapping_cycle_start_time, 2)
-print('\n', whole_scrapping_cycle_time)
+print('\n', round(time.time() - whole_scrapping_cycle_start_time, 2))
 
 
 
